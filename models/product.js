f2c9e9113c40533724c479493224fba79b2515b1
Youssef Roshdy <elroshdy23@gmail.com>
2024-09-25 21:31:01 +0300

    adding the product model and admin controller and its route

---
+++ models/product.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const path = require("path");
+const cart = require("./cart");
+
+const p = path.join(
+  path.dirname(require.mainModule.filename),
+  "data",
+  "products.json"
+);
+
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
+module.exports = class Product {
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+ 
+};
