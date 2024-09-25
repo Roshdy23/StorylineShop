ba33acd4ec0e88b13aff91c3e527ae7bd68b66cc
Youssef Roshdy <elroshdy23@gmail.com>
2024-09-25 21:43:54 +0300

    handling different requests in admin controller and download the required dependencies

--- models/product.js
+++ models/product.js
@@ -26,5 +26,51 @@ module.exports = class Product {
     this.price = price;
   }
 
- 
+  save() {
+    this.id = Math.random().toString();
+    getProductsFromFile((products) => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+
+  static findById(id, cb) {
+    getProductsFromFile((products) => {
+      const product = products.find((p) => p.id === id);
+      cb(product);
+    });
+  }
+
+  static editProductById(id, newProduct, edit) {
+    getProductsFromFile((products) => {
+      let newProducts = [];
+      let prodPrice = 0;
+      if (edit) {
+        newProducts = products.map((prod) => {
+          if (prod.id === id) {
+            prodPrice = prod.price;
+            return newProduct;
+          }
+          return prod;
+        });
+      } else {
+        newProducts = products.filter((prod) => {
+          if (prod.id === id) prodPrice = prod.price;
+          return prod.id != id;
+        });
+      }
+
+      fs.writeFile(p, JSON.stringify(newProducts), (err) => {
+        if (!err) {
+          cart.updateCart(id, prodPrice);
+        }
+      });
+    });
+  }
 };
